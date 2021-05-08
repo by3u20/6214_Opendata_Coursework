b4d7b3e22d8a4204d92547549914376fb9ae656a
Minyong Li <ml10g20@soton.ac.uk>
2021-05-08 23:02:36 +0100

    web/pref: refine form control

--- src/main/resources/static/js/pref.js
+++ src/main/resources/static/js/pref.js
@@ -45,49 +45,47 @@ const pref = {
     }
   },
   template: `
-    <div class="row g-3">
-      <div class="col-12 text-center">
-        <h1>{{ titleText }}</h1>
-      </div>
-      <div class="order-3 col-12 order-xl-1 col-xl-4 text-center">
-        <img :src="img.src" :style="img.styles">
-      </div>
-      <div class="order-1 col-12 col-lg-6 order-xl-2 col-xl-4">
-        <div class="card h-100 shadow rounded">
-          <div class="card-body">
-            <h5 class="card-title">Options</h5>
-            <div v-for="pref in getAvailPrefs" class="m-3">
-              <input type="checkbox" class="btn-check" :id="pref" autocomplete="off">
-              <label class="btn btn-outline-primary w-100" :for="pref">{{ pref }}</label>
-            </div>
-          </div>
+    <form @submit.prevent="routeToGen()">
+      <div class="row g-3">
+        <div class="col-12 text-center">
+          <h1>{{ titleText }}</h1>
         </div>
-      </div>
-      <div class="order-2 col-12 col-lg-6 order-xl-3 col-xl-4">
-        <div class="card h-100 shadow rounded">
-          <div class="card-body">
-            <h5 class="card-title">Date Range</h5>
-            <div class="row m-3 g-3">
-              <div class="col-12 col-lg-2">
-                <label for="date-from">From</label>
-              </div>
-              <div class="col-12 col-lg-10">
-                <input class="w-100" type="date" id="date-from">
-              </div>
-            </div>
-            <div class="row m-3 g-3">
-              <div class="col-12 col-lg-2">
-                <label for="date-to">To</label>
-              </div>
-              <div class="col-12 col-lg-10">
-                <input class="w-100" type="date" id="date-to">
+        <div class="order-3 col-12 order-xl-1 col-xl-4 text-center">
+          <img :src="img.src" :style="img.styles">
+        </div>
+        <div class="order-1 col-12 col-lg-6 order-xl-2 col-xl-4">
+          <div class="card h-100 shadow rounded">
+            <div class="card-body">
+              <h5 class="card-title">Options</h5>
+              <div v-for="pref in getAvailPrefs" class="m-3">
+                <input type="checkbox" class="btn-check" :id="pref" autocomplete="off">
+                <label class="btn btn-outline-primary w-100" :for="pref">{{ pref }}</label>
               </div>
             </div>
           </div>
         </div>
+        <div class="order-2 col-12 col-lg-6 order-xl-3 col-xl-4">
+          <div class="card h-100 shadow rounded">
+            <div class="card-body">
+              <h5 class="card-title">Date Range</h5>
+              <div class="row m-3 g-3">
+                <div class="col-12">
+                  <label for="date-from" class="form-label">From</label>
+                  <input type="date" id="date-from" class="form-control" required>
+                </div>
+              </div>
+              <div class="row m-3 g-3">
+                <div class="col-12">
+                  <label for="date-to" class="form-label">To</label>
+                  <input type="date" id="date-to" class="form-control" required>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="text-end mt-3">
-      <button role="button" class="btn btn-primary shadow" @click="routeToGen">{{ btnNextText }}</button>
-    </div>`
+      <div class="text-end mt-3">
+        <button type="submit" :value="btnNextText" class="btn btn-primary shadow">Next</button>
+      </div>
+    </form>`
 }
