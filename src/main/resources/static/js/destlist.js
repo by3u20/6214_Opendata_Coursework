2949842ecde31940ea9ad1692518817996ac814d
Minyong Li <ml10g20@soton.ac.uk>
2021-05-08 23:32:50 +0100

    web/destlist: adapt api, refine rank sorting

--- src/main/resources/static/js/destlist.js
+++ src/main/resources/static/js/destlist.js
@@ -3,9 +3,17 @@ const destlist = {
     return {
       titleText: "Here Is Your Destination List!",
       btnText: "Go Back",
+      destsSorted: null,
     }
   },
   props: ["dests"],
+  computed: {
+    getDests() {
+      if (!this.destsSorted)
+        this.destsSorted = JSON.parse(this.dests).sort((d1, d2) => d1.rank - d2.rank)
+      return this.destsSorted
+    }
+  },
   template: `
   <div class="row">
     <div class="col-12 text-center">
@@ -14,12 +22,22 @@ const destlist = {
     <div class="col-12">
       <div class="my-3 p-3 bg-light rounded shadow">
 
-        <!-- Sample Data -->
-        <div v-for="dest in JSON.parse(dests)" class="row fs-5">
-          <div class="col-12 col-md-2 col-xl-1">No. {{ dest.rank }}</div>
-          <div class="col-12 col-md-10 col-xl-5">{{ dest.name }}</div>
-          <div class="col-12 col-md-6 col-xl-3">{{ dest.weather }}</div>
-          <div class="col-12 col-md-6 col-xl-3">{{ dest.covid19 }}</div>
+        <div class="row fs-5">
+          <div class="col-12 col-xl-1"><b>Rank</b></div>
+          <div class="col-12 col-xl-3"><b>Name</b></div>
+          <div class="col-12 col-xl-2"><b>COVID-19 Index</b></div>
+          <div class="col-12 col-xl-2"><b>Weather</b></div>
+          <div class="col-12 col-xl-2"><b>Travel Index</b></div>
+          <div class="col-12 col-xl-2"><b>Shopping Index</b></div>
+        </div>
+
+        <div v-for="dest in getDests" class="row fs-5">
+          <div class="col-12 col-xl-1">{{ dest.rank }}</div>
+          <div class="col-12 col-xl-3">{{ dest.name }}</div>
+          <div class="col-12 col-xl-2">{{ dest.covid19 }}</div>
+          <div class="col-12 col-xl-2">{{ dest.weather }}</div>
+          <div class="col-12 col-xl-2">{{ dest.travel }}</div>
+          <div class="col-12 col-xl-2">{{ dest.shopping }}</div>
         </div>
 
       </div>
